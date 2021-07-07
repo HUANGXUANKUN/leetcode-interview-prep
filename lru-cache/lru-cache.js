28c699cf7d52c27e4c3a14f551af4b0d8a4dfa14
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-07 16:46:04 +0800

    Time: 668 ms (30.23%), Space: 93.7 MB (20.81%) - LeetHub

---
+++ lru-cache/lru-cache.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+  this.capacity = capacity;
+  // Map's entries are ordered by the insertion order.
+  this.map = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+  if (!this.map.has(key)) {
+    return -1;
+  }
+  const value = this.map.get(key);
+  this.map.delete(key);
+  this.map.set(key, value);
+  return value;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+  if (this.map.has(key)) {
+    this.map.delete(key);
+  }
+  this.map.set(key, value);
+  if (this.map.size > this.capacity) {
+    const keys = this.map.keys(); // `keys` is an iterator
+    this.map.delete(keys.next().value)
+  }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
