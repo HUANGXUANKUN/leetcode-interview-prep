9a89722c0edf89b94bf23eda70938f9dcfd626fe
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-08 13:50:15 +0800

    Time: 664 ms (33.75%), Space: 93.6 MB (23.29%) - LeetHub

--- lru-cache/lru-cache.js
+++ lru-cache/lru-cache.js
@@ -2,9 +2,9 @@
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-  this.capacity = capacity;
-  // Map's entries are ordered by the insertion order.
-  this.map = new Map();
+    this.capacity = capacity;
+    this.map = new Map(); // use map insert order to track LRU
+    
 };
 
 /** 
@@ -12,13 +12,17 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-  if (!this.map.has(key)) {
+    // console.log("getting " + key)
+    
+    if (this.map.has(key)){
+        const value = this.map.get(key);
+        //reinsert
+        this.map.delete(key);
+        this.map.set(key, value);
+        return value;
+    }
     return -1;
-  }
-  const value = this.map.get(key);
-  this.map.delete(key);
-  this.map.set(key, value);
-  return value;
+    
 };
 
 /** 
@@ -27,14 +31,22 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-  if (this.map.has(key)) {
-    this.map.delete(key);
-  }
-  this.map.set(key, value);
-  if (this.map.size > this.capacity) {
-    const keys = this.map.keys(); // `keys` is an iterator
-    this.map.delete(keys.next().value)
-  }
+    // console.log("putting " + key + ' ' + value)
+    if (this.map.has(key)){
+        //reinsert
+        this.map.delete(key);
+        this.map.set(key, value);
+    }else{
+        this.map.set(key, value);
+    }
+    // check if exceeds the capacity
+    if (this.map.size > this.capacity){
+        const popKey = this.map.keys().next().value;        
+        this.map.delete(popKey);
+        // console.log("popping " + popKey)
+        return popKey;
+    }
+    
 };
 
 /** 
