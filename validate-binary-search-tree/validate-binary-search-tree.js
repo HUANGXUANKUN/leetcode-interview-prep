1b20b92e9f5aa7ca57b946b90323e84e80b01253
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-12 03:04:00 +0800

    Time: 112 ms (16.42%), Space: 43.1 MB (47.18%) - LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    const dfs = (root, low, high) => {
+        if (root == null) return true;
+        if (root.val <= low || root.val >= high){
+            return false;
+        }
+        return dfs(root.left, low, root.val) && dfs(root.right, root.val, high)
+        
+    }
+    return dfs(root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+    
+};
