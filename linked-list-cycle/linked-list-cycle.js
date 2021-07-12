0a7f58362d2c50c14864105500e0c666cb1b647f
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-12 17:29:26 +0800

    Time: 88 ms (52.59%), Space: 41.5 MB (31.42%) - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    //slow fast pointer
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast){
+            return true;
+        }
+    }
+    if (!fast ||  !fast.next){
+        return false
+    }return true;
+};
