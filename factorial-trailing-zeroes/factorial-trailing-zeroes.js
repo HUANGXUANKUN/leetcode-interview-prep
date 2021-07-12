5a9510f6f369d0ebcd5ce524fc7d3d44221ae8e5
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-12 17:19:50 +0800

    Time: 104 ms (18.50%), Space: 40.2 MB (28.71%) - LeetHub

--- factorial-trailing-zeroes/factorial-trailing-zeroes.js
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var trailingZeroes = function(n) {
-    // find total number of factors of 5 and multiple from 1 to n
-    let power_of_5 = 5;
-    let count = 0;
+    // find number of factor of 10 from 1 to n
     
-    // n/5 + n/25 + n/125 + .... + n/largest power of 5 that <= n
-    while (power_of_5 <= n){
-        let curr_count = Math.floor(n/power_of_5); // number of mulitple of power_of_5
-        count += curr_count;
-        power_of_5 = power_of_5 * 5; // next power of 5
+    // since factor of 10 consist of 2 and 5, find the min number of factor of 2 and factor 5
+    
+    // since number of factor 5 is always smaller than factor of 2, only need to find factor of 5 in this case
+    let count = 0
+    let multiple_of_5 = 5;
+    while(multiple_of_5 <= n){
+        count += Math.floor(n/multiple_of_5)
+        multiple_of_5 *= 5;
     }
     return count;
-    
-    
-    
+
     
 };
