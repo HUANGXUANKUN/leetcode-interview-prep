98859d19f3e04415b6379a3020f0b1f4340bd513
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-19 01:16:13 +0800

    Time: 96 ms (79.72%), Space: 41.5 MB (27.68%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.js
+++ set-matrix-zeroes/set-matrix-zeroes.js
@@ -2,55 +2,56 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-
 var setZeroes = function(matrix) {
-    const n = matrix.length;
-    const m = matrix[0].length;
-    let firstRowHasZero = false;
-    let firstColHasZero = false;
-    // check first row 
+    // use first row and first col
+    let firstRowZero = false,
+        firstColZero = false;
+    const n = matrix.length,
+          m = matrix[0].length;
+    
+    // check first row
     for (let i = 0; i < m; i++){
         if (matrix[0][i] == 0){
-            firstRowHasZero = true;
+            firstRowZero = true;
             break;
         }
     }
-    // check first col 
+    // check first col
     for (let i = 0; i < n; i++){
         if (matrix[i][0] == 0){
-            firstColHasZero = true;
+            firstColZero = true;
             break;
         }
     }
-    // check matrix[2..n][2..m] is zero, if yes mark the first col or first row
+    // check 1,1 to n,m, and update first row and first col
     for (let i = 1; i < n; i++){
         for (let j = 1; j < m; j++){
-            if (matrix[i][j] == 0){
-                matrix[0][j] = 0 // mark first row -> whole col is zero
-                matrix[i][0] = 0 // mark first col -> whole row is zero
+            if(matrix[i][j] == 0){
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
             }
         }
     }
-    
-    // iterate matrix to assign zero
+    // iterate 1,1 to n,m again, assign zero
     for (let i = 1; i < n; i++){
         for (let j = 1; j < m; j++){
-            if (matrix[i][0] == 0 || matrix[0][j] == 0){
-                matrix[i][j] = 0
+            if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                matrix[i][j] = 0;
             }
         }
     }
+    // assign zero to first row and first col
+    if (firstRowZero){
+        // check first row
+        for (let i = 0; i < m; i++){
+            matrix[0][i] = 0;
+        }
+    }
+    if (firstColZero){
+        // check first row
+        for (let i = 0; i < n; i++){
+            matrix[i][0] = 0;
+        }
+    }
     
-    // iterate first col/first row to assign zero
-    if (firstRowHasZero){
-        for(let i = 0; i < m; i++){
-            matrix[0][i] = 0
-        }
-    }
-
-    if (firstColHasZero){
-        for(let i = 0; i < n; i++){
-            matrix[i][0] = 0
-        }
-    }
 };
