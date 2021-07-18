2e47e8ede285705c4cab86836cf74206681d0353
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-18 14:23:06 +0800

    Time: 92 ms (29.38%), Space: 40.4 MB (28.13%) - LeetHub

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -4,40 +4,43 @@
  * @return {boolean}
  */
 var search = function(nums, target) {
-    // trim head == tail
-    while (nums.length >= 2 && nums[nums.length-1] == nums[0]){
-        nums.pop()
+    // trim left == right
+    while (nums.length > 1 && nums[0] == nums[nums.length-1]){
+        nums.pop();
     }
+    console.log(nums);
+    // binary search
+    let left = 0,
+        right = nums.length -1;
     
-    let left = 0, right = nums.length-1;
-    // let count = 0;
     while (left < right){
-        // count++;
-        const mid = left + Math.floor(((right-left) /2));
-        // console.log("left  " + left + " right = " + right)
-        if (target == nums[mid]){
+        const mid = left + ((right-left) >> 1);
+        // check if mid is in left section or right section
+        if (nums[mid] == target){
             return true;
         }
-        // check if mid in left section
+        
         if (nums[mid] >= nums[left]){
-            // check if target to the left of mid
+            // in left section
+            // if target in [left mid] 
             if (target >= nums[left] && target < nums[mid]){
-                right = mid - 1; // left target mid | right
+                right = mid - 1;
             }else{
-                left = mid + 1; // left mid  target right
-            }
-        }
-        // if mid in right section
-        else{
-            // check if target to the right of mid
-            if (target < nums[left] && target > nums[mid]){
-                // left  | mid target right
+            // target in [mid, right]
                 left = mid + 1;
-            }else{
+            }
+        }else{
+            // in right section
+            // if target in (mid right)
+            if (target < nums[left] && target > nums[mid]){
+                left = mid + 1;
+            }
+            // target in (left, mid)
+            else{
                 right = mid - 1;
             }
         }
     }
-    
-    return nums[left] == target
+    if (nums[left] == target) return true;
+    return false;
 };
