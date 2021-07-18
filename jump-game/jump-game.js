39a5affab1acadd8ad8111ee248f6c32addaa5ab
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-18 18:02:58 +0800

    Time: 464 ms (26.01%), Space: 48.3 MB (8.53%) - LeetHub

--- jump-game/jump-game.js
+++ jump-game/jump-game.js
@@ -3,28 +3,24 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    const dp = (nums, currIndex, jumped) => {
-        const n = nums.length;
-        // base case
-        if (currIndex >= n - 1){
-            // reaching last index
-            jumped[currIndex] = true;
-            return
-        }
-        // if visited
-        if (jumped[currIndex]) return;
-        
-        jumped[currIndex] = true;
-        
-        for (let i = 1; i <= nums[currIndex]; i++){
-            // jump forward 1 to nums[curr] steps
-            dp(nums, currIndex + i, jumped)
-            if (jumped[n-1]) return true;
-        }
-    }
+    let startIndex = 0;
+    let endIndex = startIndex + nums[0];
     
-    const n = nums.length;
-    let jumped = Array(n).fill(false);
-    dp(nums, 0, jumped);
-    return jumped[n-1]
+    while (endIndex < nums.length - 1){
+        console.log("startIndex = " + startIndex)
+        console.log("endIndex = " + endIndex)
+        // iterate from start to end
+        let newEndIndex = endIndex;
+        for (let i = startIndex; i <= endIndex; i++){
+            newEndIndex = Math.max(newEndIndex, i + nums[i])
+        }
+        // check if can move forward
+        if (endIndex == newEndIndex){
+            // never moved 
+            return false;
+        }
+        startIndex = endIndex +1;
+        endIndex = newEndIndex;
+    }
+    return true;
 };
