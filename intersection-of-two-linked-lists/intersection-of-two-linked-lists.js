e003ec2cb10e01a60be36a1018a21f8f02630739
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-18 10:45:10 +0800

    Time: 140 ms (21.69%), Space: 45.7 MB (67.67%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,23 +12,22 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    // after a finish, continue on b
-    // after b finish, continue on a
-    // a= a + c + b -> reach intersect or null
-    // b= b + c + a -> reach intersect or null
-    
-    let nodeA = headA, nodeB = headB;
+    // headA then headB
+    // headB then headA
+    let nodeA = headA;
+    let nodeB = headB;
     while (nodeA != nodeB){
-        if(nodeA == null){
+        if(nodeA == null) {
             nodeA = headB;
         }else{
             nodeA = nodeA.next;
         }
-        if(nodeB == null){
+        if(nodeB == null) {
             nodeB = headA;
         }else{
             nodeB = nodeB.next;
         }
-    }
+    } 
     return nodeA;
+
 };
