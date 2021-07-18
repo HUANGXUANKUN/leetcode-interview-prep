bc279141881b52d92569c69db057e67ac91b832a
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-19 01:36:00 +0800

    Time: 80 ms (46.45%), Space: 41.9 MB (5.10%) - LeetHub

--- rotate-image/rotate-image.js
+++ rotate-image/rotate-image.js
@@ -3,24 +3,25 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    const transpose = (matrix) =>{
-        const n = matrix.length, m = matrix[0].length;
-        for(let i = 0; i < n; i++){
+    const transpose = (matrix) => {
+        const n = matrix.length;
+        for (let i = 0; i < n; i++){
             for (let j = 0; j <= i; j++){
-                [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]; 
+                [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
             }
         }
+        console.log(matrix);
     }
-    
-    const revert = matrix => {
-        const n = matrix.length, m = matrix[0].length;
-        for(let i = 0; i < n; i++){
-            for(let j = 0; j < Math.floor(m/2); j++){
-                [matrix[i][m-1-j], matrix[i][j]] = [matrix[i][j], matrix[i][m-1-j]]
+    const revert = (matrix) => {
+        const n = matrix.length;
+        for (let i = 0; i < n; i++){
+            for (let j = 0; j < (n >> 1); j++){
+                [matrix[i][j], matrix[i][n-1-j]] = [matrix[i][n-1-j], matrix[i][j]];         
             }
         }
+        console.log(matrix);
     }
-    
     transpose(matrix);
     revert(matrix);
+
 };
