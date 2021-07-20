42f8195f8799de1d23ab24f1c8bde92b13b7606b
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-20 19:22:04 +0800

    Time: 116 ms (69.42%), Space: 60.3 MB (14.72%) - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.js
+++ contains-duplicate-ii/contains-duplicate-ii.js
@@ -4,19 +4,14 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    // hashmap to record index
     const map = new Map();
     for (let i = 0; i < nums.length; i++){
-        const curr = nums[i]
-        // check duplicate 
-        if (map.has(curr)){
-            // check index
-            if (i - map.get(curr) <= k){
-                return true
-            }
+        const curr = nums[i];
+        // check if existed
+        if(map.has(curr)){
+            if (i - map.get(curr) <= k) return true;
         }
         map.set(curr, i);
     }
     return false;
-    
 };
