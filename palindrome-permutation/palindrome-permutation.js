34c7b3591bd4e6472828df9a5af9ab6362ce4b32
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-21 10:54:53 +0800

    Time: 60 ms (98.86%), Space: 39 MB (8.37%) - LeetHub

--- palindrome-permutation/palindrome-permutation.js
+++ palindrome-permutation/palindrome-permutation.js
@@ -3,22 +3,22 @@
  * @return {boolean}
  */
 var canPermutePalindrome = function(s) {
-    // check if all count are double
+    // check the number of odd count character
+    const map = new Map();
+    let odd = 0;
     
-    // there can only be one odd
-    const letterArray = Array(26).fill(0);
     for (const char of s){
-        const index = char.charCodeAt() - 'a'.charCodeAt();
-        letterArray[index] += 1;
+        map.set(char, (map.get(char) || 0) + 1);
     }
-    let countOdd = 0;
-    for (const letterCount of letterArray){
-        if (letterCount & 1 == 1){
-            // it is odd
-            countOdd += 1;
+    for (const count of map.values()){
+        if (count & 1 == 1){
+            odd += 1;
         }
     }
-    return countOdd <= 1 ? true : false;
+    if (odd > 1) return false;
+    return true;
     
+    // if odd count <= 1, then true
+    // else false
     
 };
