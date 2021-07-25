9648df971e9b8f0e67af1dbd91bdbaba2f80a5a0
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-07-25 22:57:16 +0800

    Time: 56 ms (99.93%), Space: 40.3 MB (38.95%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // consider the trending graph
-    
-    // cumulate all increasing trend
-    let profit = 0;
-    for (let i = 0; i < prices.length - 1; i++){
-        // trending is increasing the next day, buy
-        if(prices[i] < prices[i+1]){
-            profit += prices[i+1] - prices[i];
+    // sum all the increasing trend
+    let sum = 0;
+    for (let i = 0; i < prices.length-1; i++){
+        // check if [i, i+1] is increasing trend
+        if (prices[i+1] > prices[i]){
+            sum+=prices[i+1] - prices[i]
         }
     }
-    return profit;
+    return sum;
     
 };
