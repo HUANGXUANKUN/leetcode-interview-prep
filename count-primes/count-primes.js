8c95abebad7af23108738859d7696f128ec5645e
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-03 06:49:31 +0800

    Time: 280 ms (62.61%), Space: 79.2 MB (68.58%) - LeetHub

--- count-primes/count-primes.js
+++ count-primes/count-primes.js
@@ -3,38 +3,23 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-    if (n< 2){
-        return 0;
-    }
-    
-    const primeMap = {};
-    // from 2 to n,
-    // if i not in prime set -> set primeSet[i] to true
-    // then set all multiple of i as primeSet[i] = false 
-    // else if in primeset = true, continue
-    // count
-    // console.log(Math.sqrt(n))
-    
-    let notPrimeCount = 0;
-    for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++){
-        if (!(i in primeMap)){
-            // it is prime
-            // primeMap.add(i);
-            // set all multiple of i to false
-            let multiple_of_prime = i+i;
-            for (let j = i+i; j < n; j += i){
-                if (!(j in primeMap)){
-                    notPrimeCount += 1;  
-                    primeMap[j] = 1;
-                }
-                // console.log("at j = " + j)
+    // start from 2
+    if (n <= 1) return 0;
+    const checked = Array(n).fill(0);
+    let primeCount = 0;
+    for (let i = 2; i < n; i++){
+        // if not checked, is prime
+        if (checked[i] == 0){
+            checked[i] = 1;
+            primeCount++;
+            // check all multiple of prime
+            for (let j = i+i; j < n; j+=i){
+                checked[j] = 1;
             }
         }
+        // if checked, is not a prime
     }
-    // console.log(primeMap);
-    // exclude "1", "n" and non-prime
-    // console.log(notPrimeCount)
-    return n-2-notPrimeCount;
+    return primeCount;
+    
+    
 };
-
-// 1 2 3 /4 5 /6 7 /8 /9
