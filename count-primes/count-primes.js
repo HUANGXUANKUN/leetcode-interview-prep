66be45a8003e08cf78d6dc4e8583ff4eb31b9651
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-03 07:00:27 +0800

    Time: 224 ms (84.70%), Space: 79.3 MB (57.01%) - LeetHub

--- count-primes/count-primes.js
+++ count-primes/count-primes.js
@@ -7,18 +7,20 @@ var countPrimes = function(n) {
     if (n <= 1) return 0;
     const checked = Array(n).fill(0);
     let primeCount = 0;
-    for (let i = 2; i < n; i++){
+    for (let i = 2; i <= Math.sqrt(n); i++){
         // if not checked, is prime
         if (checked[i] == 0){
-            checked[i] = 1;
-            primeCount++;
             // check all multiple of prime
-            for (let j = i+i; j < n; j+=i){
+            for (let j = i*i; j < n; j+=i){
                 checked[j] = 1;
             }
         }
         // if checked, is not a prime
     }
+    for (let i = 2; i < n; i++){
+        if (checked[i] == 0) primeCount++;
+    }
+
     return primeCount;
     
     
