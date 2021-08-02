7f412eae1075daddb09a8a7f3a921310d36c89b0
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-03 05:13:46 +0800

    Time: 76 ms (53.62%), Space: 38.7 MB (56.57%) - LeetHub

--- rotate-string/rotate-string.js
+++ rotate-string/rotate-string.js
@@ -4,12 +4,16 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-    if (s.length != goal.length) return false;
-    // join s and goal
-    const newS = s + s;
-    // check if newS contains goal
-    if (newS.indexOf(goal) != -1){
+    // check length
+    const n = s.length,
+          m = goal.length;
+    if (n != m) return false;
+    // compare s+s with goal
+    const s_s = s+s;
+    if (s_s.indexOf(goal) != -1){
         return true;
     }
     return false;
+    
+    
 };
