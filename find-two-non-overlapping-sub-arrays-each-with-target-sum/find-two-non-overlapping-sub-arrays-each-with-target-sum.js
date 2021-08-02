f7798131757265e0795d52f35ec9c3fa6a431bc7
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-03 05:57:50 +0800

    Time: 176 ms (51.16%), Space: 59.1 MB (41.86%) - LeetHub

--- find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.js
+++ find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.js
@@ -4,72 +4,48 @@
  * @return {number}
  */
 var minSumOfLengths = function(arr, target) {
-    // minLen sub array from the left
-    const n = arr.length;
-    const minLenFromLeft = Array(n).fill(Number.POSITIVE_INFINITY);
-    let minLen = Number.POSITIVE_INFINITY;
-    let left = 0;
-    let currSum = 0;
-    for (let right = 0; right < n; right++){
-        currSum += arr[right];
-        // if shrink
-        while (left < right && currSum > target){
-            // popleftmost
-            const leftNum = arr[left];
-            left++;
-            currSum -= leftNum;
+    const getMinLenArray = (arr, target) => {
+        const n = arr.length;
+        let left = 0;
+        const minLengthFromLeft = Array(n).fill(Number.POSITIVE_INFINITY);
+        let minLen = Number.POSITIVE_INFINITY;
+        let windowSum = 0;
+        for (let right = 0; right < n; right++){
+            windowSum += arr[right];
+            // check if need shrink
+            while (left <= right && windowSum > target){
+                windowSum -= arr[left];
+                left++;
+            }
+            if (windowSum == target){
+                minLen = Math.min(minLen, right-left+1);
+            }
+            minLengthFromLeft[right] = minLen;
         }
-        // console.log("at right = " + right + " currSum = " + currSum);
-        
-        if (currSum == target){
-            // record len
-            // console.log("Found ! at left = " + left + " right = " + right)
-            const len = right-left+1;
-            minLen = Math.min(len, minLen);
-        }
-        minLenFromLeft[right] = minLen;
-        
+        return minLengthFromLeft;
     }
-    // console.log(minLenFromLeft);
-    // minLen sub array from the right
-    const minLenFromRight = Array(n).fill(Number.POSITIVE_INFINITY);
-    minLen = Number.POSITIVE_INFINITY;
-    currSum = 0;
-    let right = n-1;
     
-    for (let left = n-1; left >= 0 ; left--){
-        currSum += arr[left];
-        // if shrink
-        while (left < right && currSum > target){
-            // popRightMost
-            const rightNum = arr[right];
-            right--;
-            currSum -= rightNum;
-        }
-        
-        if (currSum == target){
-            // record len
-            // console.log("Found ! at left = " + left + " right = " + right)
-            const len = right-left+1;
-            minLen = Math.min(len, minLen);
-        }
-        minLenFromRight[left] = minLen;
-    }
-    // console.log(minLenFromRight)
-    // find the minLen from [1, pivot] to [pivot+1, n]
-    let minLenOfTwo = Number.POSITIVE_INFINITY; 
-    for(let i = 0; i < n-1; i++){
-        const leftMin = minLenFromLeft[i];
-        const rightMin = minLenFromRight[i+1];
-        if (leftMin != Number.POSITIVE_INFINITY && rightMin != Number.POSITIVE_INFINITY){
-            // has result
-            minLenOfTwo = Math.min(minLenOfTwo, leftMin+rightMin);
-        }
-    }
-    if (minLenOfTwo == Number.POSITIVE_INFINITY){
-        // not found
+    // base case
+    if (arr.length == 1){
         return -1;
     }
-    return minLenOfTwo;
-    
+    // use sliding window from left
+    const minArrFromLeft = getMinLenArray(arr, target);
+    // use sliding window from right
+    const reverseArr = arr.slice(0).reverse();
+    const minArrFromRight = getMinLenArray(reverseArr, target).reverse();
+    console.log(minArrFromLeft);
+    console.log(minArrFromRight);
+
+    // length is atleast 2
+    let result = Number.POSITIVE_INFINITY;
+    for (let i = 0; i < arr.length-1; i++){
+        // check if valid for [0...i] and [i+1...n-1]
+        if (minArrFromLeft[i] != Number.POSITIVE_INFINITY &&
+            minArrFromRight[i+1] != Number.POSITIVE_INFINITY){
+            result = Math.min(result, minArrFromLeft[i]+minArrFromRight[i+1]);
+        }
+    }
+    if (result == Number.POSITIVE_INFINITY) return -1;
+    return result;
 };
