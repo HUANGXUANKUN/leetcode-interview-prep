13791770120090affd22788bf3a1b82f1a87af66
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-03 20:00:23 +0800

    Time: 516 ms (24.79%), Space: 48.8 MB (8.21%) - LeetHub

--- jump-game/jump-game.js
+++ jump-game/jump-game.js
@@ -3,19 +3,25 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    // start from end position, look for posible jumper 
     const n = nums.length;
-    if (n <= 1) return true;
-    let targetIndex = n-1;
-    for (let i = n-2; i >= 0; i--){
-        // check if possible
-        const jumpDist = nums[i];
-        if (i + jumpDist >= targetIndex){
-            // can jump, update targetIndex
-            if (i == 0) return true; // at start position
-            targetIndex = i;
+    let currIndex = 0;
+    while(true){
+        // jump dist from currIndex
+        let jumpDist = nums[currIndex];
+        if (currIndex + jumpDist >= nums.length - 1) return true; // jump over the end
+        let furthest = currIndex;
+        let newIndex = currIndex;
+        for (let i = currIndex; i <= currIndex + jumpDist; i++){
+            // update next index if it can jump furthest
+            const nextIndex = i + nums[i];
+            if (nextIndex > furthest){
+                furthest = nextIndex;
+                newIndex = i;
+            }
         }
+        console.log([currIndex, newIndex])
+        // if nothing change, break and return false
+        if (newIndex == currIndex) return false;
+        currIndex = newIndex;
     }
-    return false;
-    
 };
