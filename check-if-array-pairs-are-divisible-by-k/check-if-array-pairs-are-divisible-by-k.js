19985d02a4d7ca2b196affa92cb2e5f0443f5073
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-04 21:24:54 +0800

    Time: 140 ms (52.94%), Space: 54.7 MB (35.29%) - LeetHub

--- check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.js
+++ check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.js
@@ -4,36 +4,36 @@
  * @return {boolean}
  */
 var canArrange = function(arr, k) {
-    // check if zero divisible
-    // if zero divisible by k
-    const remainder = Array(k).fill(0);
-    
+    // n is even
+    const remainderCounter = Array(k).fill(0);
     for (const num of arr){
-        // get remainder 
-        let r = num % k;
-        if (r < 0){
-            r = (r + k) % k; // if r = -2, r = (-2+k) % k
-        }
-        remainder[r] += 1;
+        // get remainder
+        let remainder = num % k;
+
+        // to account for negative
+        remainder = (remainder + k) % k;
+        // console.log(num, remainder)
+        remainderCounter[remainder] += 1;
     }
-    
-    // check numbers with r = 0, which itself form a pair
-    if (remainder[0] & 1 == 1){
-        // is odd, fail
+    console.log(remainderCounter)
+    // check if remiander[0] is even
+    if (remainderCounter[0] & 1 == 1){
         return false;
     }
-    // check numbers with r = k/2
-    if (remainder[k/2] & 1 == 1){
-        // is odd, fail
+    // if k is even, check if remainder[k/2] is even
+    const half_k = k >> 1;
+    if (k & 1 == 0 && remainderCounter[half_k] & 1 == 0){
         return false;
     }
-    
-    // check all other pair
-    for (let i = 1; i < k/2; i++){
-        const target = k-i;
-        if (remainder[i] != remainder[target]){
-            return false;
-        }        
+    // k = 5, remainder = 0, 1, 2, 3, 4
+    // 0, 1, 2, 3
+    // check i from 1 to k/2 -1 
+    for (let i = 1; i <= Math.floor(k/2); i++){
+        const oppositeRemainder = k - i;
+       // console.log([i, oppositeRemainder], [remainderCounter[i], remainderCounter[oppositeRemainder]])
+        
+        if (remainderCounter[i] != remainderCounter[oppositeRemainder]) return false;
     }
     return true;
-};
+}
+    
