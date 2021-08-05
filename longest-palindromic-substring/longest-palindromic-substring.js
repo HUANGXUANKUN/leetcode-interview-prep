1be066f074563d46c37090378a307b12a6ae004a
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-06 06:56:22 +0800

    Time: 100 ms (82.97%), Space: 46.9 MB (18.57%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.js
+++ longest-palindromic-substring/longest-palindromic-substring.js
@@ -3,43 +3,40 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    const expand = (s, left, right) => {
-        if (right >= s.length || s[left] != s[right]) return [left, left];
-        let start = left,
-            end = right;
-        while(left >= 0 && right < s.length && s[left] == s[right]){
-            start = left;
-            end = right;
-            left--;
-            right++;
+    // parlindrome1: a|ab|b
+    // palindrome2: aba
+    const expandFromTwoPointers = (s, left, right)=>{
+        const n = s.length;
+        if (s[left] != s[right]) return [left, left];  //eg aabcaa
+        // try expanding
+        while(true){
+            // check the next left and next right
+            if (left - 1 < 0 || right + 1 >= n || s[left-1] != s[right+1]){
+                return [left, right];
+            }
+            left -= 1;
+            right += 1;
         }
-        return [start, end];
     }
-    // expand from center, start from i, i or (i-1, i)
     let maxLen = 0;
-    let start = 0, end = 0;
+    let resultLeft = 0, resultRight = 0;
     for (let i = 0; i < s.length; i++){
-        console.log("i = " + i)
-        // expand from i,i
-        const result1 = expand(s, i, i);
-        // expand from i, i+1
-        const result2 = expand(s, i, i+1);
-
+        // expand from the curr char
+        const arr1 = expandFromTwoPointers(s, i, i); //eg aba
         
-        if (result1[1] - result1[0] + 1 > maxLen){
-            maxLen = result1[1] - result1[0] + 1;
-            start = result1[0], end = result1[1];      
-            // console.log(result1)
-            
+        const arr2 = i == s.length - 1 ? [i, i] : expandFromTwoPointers(s, i, i+1);
+        // console.log([arr1, arr2])
+        const len1 = arr1[1] - arr1[0] + 1;
+        const len2 = arr2[1] - arr2[0] + 1;
+        if (len1 > maxLen){
+            resultLeft = arr1[0], resultRight = arr1[1];
+            maxLen = len1;
         }
-        if (result2[1] - result2[0] + 1 > maxLen){
-            maxLen = result2[1] - result2[0] + 1;
-            start = result2[0], end = result2[1];    
-            // console.log(result2)
+        if (len2 > maxLen){
+            resultLeft = arr2[0], resultRight = arr2[1];
+            maxLen = len2;
         }
     }
-    
-    // console.log([start, end])
-    return s.slice(start, end + 1)
-    
+    console.log(resultLeft, resultRight);
+    return s.slice(resultLeft, resultRight+1);
 };
