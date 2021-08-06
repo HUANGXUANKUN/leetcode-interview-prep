3237fe560441ab8a05ec583d2f4caef8f1f0433e
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-07 00:49:28 +0800

    Time: 84 ms (84.48%), Space: 47.9 MB (59.50%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -3,25 +3,30 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    // two pointers
-    let left = 0, right = height.length-1;
+    // O(N)
+    
+    // Height of the tank is determined by the lowest wall (left or right)
+    
+    // we should increment or decrement the pointers which is lower 
+    let left = 0;
+    let right = height.length - 1;
     let maxSize = 0;
-    while (left < right){
-        // find the lowest one
-        const len = right-left;
-        const minHeight = Math.min(height[left], height[right]);
+    while(left < right){
+        // console.log([left, right]);
+        // look for the lower pointer and update it
+        const heightLeft = height[left],
+              heightRight = height[right];
+        // console.log("height: " + [heightLeft, heightRight]);
         
-        maxSize = Math.max(maxSize, len * minHeight);
-        // console.log("left = " + left + " right = " + right)
-        // console.log("len = " + len)
-        // console.log("minHeight = " + minHeight)
-        // console.log(maxSize);
-        if (height[left] < height[right]){
-            left++;
+        const width = right - left; // this is the width of the tank
+        const size = width * Math.min(heightLeft, heightRight);
+        // console.log("size = " + size);
+        maxSize = Math.max(maxSize, size); // update the size
+        if (heightLeft <= heightRight ){
+            left ++;
         }else{
-            right--;
+            right --;
         }
     }
     return maxSize;
-    
 };
