c99446a78873fecb354e8ce4740d1e416458987f
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-07 00:35:48 +0800

    Time: 80 ms (91.64%), Space: 45.1 MB (68.39%) - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -3,13 +3,15 @@
  * @return {string}
  */
 var reverseWords = function(s) {
+    // step 1: tokenize s into words
     const tokens = s.split(' ');
-    // revert each tokens
-    for(let i = 0; i < tokens.length; i++){
-        const chars = tokens[i].split('');
-        chars.reverse();
-        tokens[i] = chars.join('');
+    // step 2: reverse each word
+    for (let i = 0; i < tokens.length; i++){
+        const characters = tokens[i].split('');
+        characters.reverse();
+        tokens[i] = characters.join('');
     }
-    return tokens.join(' ')
+    // step 3: join the reversed word back to a new sentence
+    return tokens.join(' ');
     
 };
