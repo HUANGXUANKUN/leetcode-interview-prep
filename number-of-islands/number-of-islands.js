32a74b287ebe8d48967d4f426aa67f345e2eb7fc
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-18 02:20:22 +0800

    Time: 76 ms (90.43%), Space: 41.4 MB (48.12%) - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -3,27 +3,30 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const bfs = (grid, i, j, n, m) => {
-        // visit all '1', and change it to '0'
-        if (i < 0 || j < 0 || i >= n || j>= m){
-            return; //out of bound
+    const dfs = (grid, i, j) => {
+        const n = grid.length,
+              m = grid[0].length;
+        
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0'){
+            return;
         }
-        if (grid[i][j] == '0') return; // visited
-        grid[i][j] = '0';
-        // visit 4 direction
-        bfs(grid, i+1, j, n, m)
-        bfs(grid, i-1, j, n, m)
-        bfs(grid, i, j+1, n, m)
-        bfs(grid, i, j-1, n, m)
+        grid[i][j] = '0'; // make current cell water (block from revisiting)
+        // dfs left right top bottom
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
     }
+    
+    // dfs
+    let count = 0;
     const n = grid.length,
           m = grid[0].length;
-    let count = 0;
     for (let i = 0; i < n; i++){
         for(let j = 0; j < m; j++){
-            if (grid[i][j] == '1'){
-                count+=1;
-                bfs(grid, i, j, n, m)
+            if(grid[i][j] == '1'){
+                count++;
+                dfs(grid, i, j);
             }
         }
     }
