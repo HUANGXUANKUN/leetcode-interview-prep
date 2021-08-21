cae246ac00bf0568d19eb2d134006a7f9910b7d2
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-22 03:13:50 +0800

    Time: 64 ms (99.27%), Space: 40.1 MB (75.39%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+    let temp = 0;
     let maxSum = Number.NEGATIVE_INFINITY;
-    let currSum = 0;
-    for (const num of nums){
-        if (currSum + num > num){
-            currSum += num;
-        }
-        else{
-            currSum = num
-        };
-        maxSum = Math.max(currSum, maxSum);
+    for (let right = 0; right < nums.length; right++){
+        const curr = nums[right];
+        temp += curr;
+        maxSum = Math.max(maxSum, temp);
+        if(temp <= 0) temp = 0;
     }
     return maxSum;
+    
 };
