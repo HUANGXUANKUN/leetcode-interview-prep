1d9904e7330ed42083767fb31c018d9ba0a72610
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-22 03:19:40 +0800

    Time: 88 ms (97.16%), Space: 41 MB (93.68%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let result = 0;
-    const map = new Map();
     let left = 0;
+    const map = new Map();
+    let maxLen = 0;
     for (let right = 0; right < s.length; right++){
         const curr = s[right];
-        if (map.has(curr)){
-            // already exists, update left
-            const prevIndex = map.get(curr);
-            left = Math.max(left, prevIndex+1);
+        if(map.has(curr)){
+            // check if last index is in window
+            const lastIdx = map.get(curr);
+            if(lastIdx >= left) left = lastIdx+1;
         }
         map.set(curr, right);
-        result = Math.max(result, right-left+1);
-        // console.log("at right = " + right, [left, right, result])
-        
+        maxLen = Math.max(right-left+1, maxLen);
     }
-    return result;
+    return maxLen;
     
 };
