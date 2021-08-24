50e67be767e8f23bc02fe3bbc976408a5c636178
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-24 18:19:09 +0800

    Time: 356 ms (65.28%), Space: 42.1 MB (62.46%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
@@ -4,16 +4,21 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    // start from bottom left
     const n = matrix.length,
           m = matrix[0].length;
-    let i = n-1, j = 0;
+    let i = n-1, j = 0
     while(i >= 0 && j < m){
-        if (matrix[i][j] == target)return true;
-        else if (matrix[i][j] < target){
-            j++; // target larger, not in this column
+        const curr = matrix[i][j];
+        if (curr == target){
+            return true;
+        }
+        if (curr < target){
+            // next column
+            j++;
         }else{
-            i--; // target smaller, not in this row
+            // target < curr
+            // top row
+            i--;
         }
     }
     return false;
