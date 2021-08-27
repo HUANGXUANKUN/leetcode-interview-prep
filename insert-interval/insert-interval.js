086a7950cf110832c29520c16cc3cfee58860e47
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-08-27 20:02:56 +0800

    Time: 139 ms (7.86%), Space: 40.9 MB (80.13%) - LeetHub

--- insert-interval/insert-interval.js
+++ insert-interval/insert-interval.js
@@ -4,55 +4,42 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    // find the insertion index where newInterval.start > oldIntervals.end
-    const newStart = newInterval[0];
-    const newEnd = newInterval[1];
-    let insertIdx = 0;
+    let i = 0;
+    let newStart = newInterval[0];
+    let newEnd = newInterval[1];
     const res = [];
-    for (let i = 0; i < intervals.length; i++){
-        const oldStart = intervals[i][0];
-        const oldEnd = intervals[i][1];
-        insertIdx = i;
-        if (newStart <= oldEnd){
-            // insert from i + 1
+    while (i < intervals.length){
+        const end = intervals[i][1];
+        if (newStart <= end){
+            // break
             break;
         }else{
-            // newStart >= oldEnd
-            res.push([oldStart, oldEnd]);
-            if (i == intervals.length - 1){
-                insertIdx = i+1; // insert at last
-            }
-        }
-    }
-    if (insertIdx == intervals.length){
-        res.push([newStart, newEnd]);
-        return res;
-    }
-    // console.log(res)
-    // console.log(insertIdx)
-    let currStart = newStart;
-    let currEnd = newEnd;
-    let i = insertIdx;
-    while (i < intervals.length){
-        const oldStart = intervals[i][0];
-        const oldEnd = intervals[i][1];
-        // check if merge
-        if(!(oldStart > currEnd)){
-            // console.log("merge at i = ", i)
-            // console.log([currStart, currEnd, oldStart, oldEnd])
-            // merge
-            currStart = Math.min(currStart, oldStart);
-            currEnd = Math.max(currEnd, oldEnd);
-        }else{
-            // split, currStart > oldEnd
-            // console.log("pushing ", [currStart, currEnd])
-            res.push([currStart, currEnd]);
-            currStart = oldStart;
-            currEnd = oldEnd;
+            res.push(intervals[i]);
         }
         i++;
     }
-    // push last group
-    res.push([currStart, currEnd]);
+    // insert from i 
+    while(i < intervals.length){
+        const oldStart = intervals[i][0];
+        const oldEnd = intervals[i][1];
+        // if cannot merge
+        //    ----
+        // ----    ----  -----
+        if (oldStart > newEnd){
+            res.push([newStart, newEnd]);
+            // update
+            newStart = oldStart;
+            newEnd = oldEnd;
+        }else{
+            // else merge
+            newStart = Math.min(newStart, oldStart);
+            newEnd = Math.max(newEnd, oldEnd);
+        }
+        i++;
+    }
+    // push the final newStart, newEnd
+    res.push([newStart,newEnd]);
     return res;
+    
+    
 };
